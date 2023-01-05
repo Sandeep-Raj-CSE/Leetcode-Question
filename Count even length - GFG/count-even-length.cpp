c76e8fb776d3d60495a9a37de187c0db4ece2f0e
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-01-05 14:42:35 +0530

    Added solution - LeetHub

--- Count even length - GFG/count-even-length.cpp
+++ Count even length - GFG/count-even-length.cpp
@@ -6,35 +6,51 @@ using namespace std;
 
 class Solution{
  public:
- int m = 1e9+7;
- long long binpow(long long a, long long b) {
-        a %= m;
-        long long res = 1;
-        while (b > 0) {
-            if (b & 1)
-                res = res * a % m;
-            a = a * a % m;
-            b >>= 1;
-        }
-        return res;
-    }
-    long long inv(int n) {
-        return binpow(n, m-2);
-    }
- int compute_value(int n)
- {
-     long long ans = 1, num = 1, den = 1, cur = n;
-     for(int i = 1, j = n; i <= n; i++, j--) 
-     {
-         num = (num * j) % m;
-         den = (den * i) % m;
+//  int m = 1e9+7;
+//  long long binpow(long long a, long long b) {
+//         a %= m;
+//         long long res = 1;
+//         while (b > 0) {
+//             if (b & 1)
+//                 res = res * a % m;
+//             a = a * a % m;
+//             b >>= 1;
+//         }
+//         return res;
+//     }
+//     long long inv(int n) {
+//         return binpow(n, m-2);
+//     }
+//  int compute_value(int n)
+//  {
+//      long long ans = 1, num = 1, den = 1, cur = n;
+//      for(int i = 1, j = n; i <= n; i++, j--) 
+//      {
+//          num = (num * j) % m;
+//          den = (den * i) % m;
          
-         long long temp = (num * inv(den)) % m;
-         temp = binpow(temp, 2);
-         ans = (ans + temp) % m;
-     }
-     return (int)ans;
- }
+//          long long temp = (num * inv(den)) % m;
+//          temp = binpow(temp, 2);
+//          ans = (ans + temp) % m;
+//      }
+//      return (int)ans;
+//  }
+
+
+
+	int mod = 1000000007;
+	long long inv(long long int r){
+	    if(r==1) return 1;
+	    return (mod-((mod/r)*inv(mod%r))%mod+mod)%mod;
+	}
+	int compute_value(int n){
+	    long long int ans =1, ncr=1;
+	    for(int i=1;i<=n;i++){
+	        ncr=(((ncr*(n+1-i))%mod)*inv(i))%mod;
+	        ans=(ans+(ncr*ncr)%mod)%mod;
+	    }
+	    return ans%mod;
+	}
 };
 
 //{ Driver Code Starts.
