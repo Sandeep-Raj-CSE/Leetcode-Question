6f79d75adf8bf91aa83e59d7cc7add64357d661d
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-02-06 15:36:38 +0530

    Added solution - LeetHub

---
+++ Kth LSB - GFG/kth-lsb.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int KthLSB(long long int n, int k){
+	    //complete the function here
+	    int ans=(n>>(k-1))&1;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        int k;
+        cin >> n >> k;
+        Solution ob;
+        
+        cout << ob.KthLSB(n, k) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
