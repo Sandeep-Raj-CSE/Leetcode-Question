7aad3b1d3538334195a8ba28b86eeb3efc1da86c
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-05-10 16:54:14 +0530

    Added solution - LeetHub

---
+++ Inorder Traversal - GFG/inorder-traversal.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> inOrder(Node* curr) {
+
+        // Your code here
+
+        // vector<int> ans;
+
+        // vector<int> l;
+
+        // vector<int> r;
+
+        // if(root==NULL)
+
+        //   return ans;
+
+        //   l=inOrder(root->left);
+
+        //   r=inOrder(root->right);
+
+        // for(int i=0;i<l.size();i++)
+
+        //   ans.push_back(l[i]);
+
+        // ans.push_back(root->data);
+
+        // for(int i=0;i<r.size();i++)
+
+        //   ans.push_back(r[i]);
+
+        // return ans;  
+        
+        
+        vector < int > inOrder;
+  stack < Node * > s;
+  while (true) {
+    if (curr != NULL) {
+      s.push(curr);
+      curr = curr -> left;
+    } else {
+      if (s.empty()) break;
+      curr = s.top();
+      inOrder.push_back(curr -> data);
+      s.pop();
+      curr = curr -> right;
+    }
+  }
+  return inOrder;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        Solution ob;
+        vector<int> res = ob.inOrder(root);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
